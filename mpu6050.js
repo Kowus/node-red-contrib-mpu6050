c0585345c6398f7ae67f70cb21167fe934e50d4e
Kowus <barnabas@elite-education.org>
2019-07-24 15:23:09 +0000

    resolve #1, installed raspi-io, enabled gpio, exprt board objects

--- mpu6050.js
+++ mpu6050.js
@@ -1,9 +1,13 @@
-var five = require('johnny-five');
-var board = new five.Board();
 module.exports = function(RED) {
   function MPU6050(config) {
     RED.nodes.createNode(this, config);
     var node = this;
+    var Raspi = require('raspi-io').RaspiIO;
+    var five = require('johnny-five');
+    var board = new five.Board({
+      io: new Raspi()
+    });
+
     board.on('ready', function() {
       var imu = new five.IMU({
         controller: 'MPU6050'
